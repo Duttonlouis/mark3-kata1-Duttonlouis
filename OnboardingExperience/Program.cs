c1cab867baf3da94cde1b8624ad3f3c514593f17
LouisDutton <Duttonlouis@gmail.com>
2018-04-11 10:36:05 -0500

    starting First and Last Name

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -6,9 +6,22 @@ namespace OnboardingExperience
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello thank you for using our app to get started i need some information from you.");
+            Console.WriteLine("Please tell me your first name.");
+            var firstName = Console.ReadLine();
 
-            Console.ReadLine();
+            Console.WriteLine("Thanks your first name is " + (firstName));
+
+            Console.WriteLine("Now please tell me your last name.");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Thanks your last name is " + (lastName));
+
+
+
+
+
+
         }
     }
 }
